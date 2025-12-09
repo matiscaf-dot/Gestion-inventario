9453feb9168df6fdb8ff79697d50c42a2e23863a
matiscaf-dot <matis.caf@gmail.com>
2025-12-09 00:07:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import json
 from io import BytesIO
 from PIL import Image
 from supabase import create_client
-
+import productos
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
