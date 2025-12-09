3a3b8483d77416e4e27614beae980e4cb49760d5
matiscaf-dot <matis.caf@gmail.com>
2025-12-09 11:39:46 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,10 +32,7 @@ os.makedirs(CAPTURAS_DIR, exist_ok=True)
 # ==============================
 # FUNCIONES AUXILIARES
 # ==============================
-from supabase import create_client
-    SUPABASE_URL = st.secrets["SUPABASE_URL"]
-    SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
-    supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 def cargar_usuarios_supabase():
     """
     Consulta la tabla 'usuarios' en Supabase y devuelve un diccionario
